395a8421ece452221344f7413f393db89ac9239d
Al Tobey <tobert@gmail.com>
2013-10-28 17:25:31 -0700

    intermediate commit, non-working

--- posts.go
+++ posts.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"time"
 	"tux21b.org/v1/gocql/uuid"
@@ -17,16 +16,25 @@ type Post struct {
 }
 
 func getPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("unimplemented route: getPost()\n")
+	q := cass.Query(`SELECT id, body, dttm, authors, tags FROM posts WHERE id=?`)
+	p := Post{}
+	q.Scan(&p.Id, &p.Body, &p.Dttm, &p.Authors, &p.Tags)
+	json, _ := json.Marshal(p)
+	w.Write(json)
 }
 
 func newPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("unimplemented route: newPost()\n")
+	// get JSON, parse it, write to C*
+	p := Post{}
+	q := cass.Query(`INSERT INTO posts (id, body, dttm, authors, tags) VALUES (?, ?, ?, ?, ?)`)
+	q.Scan(&p.Id, &p.Body, &p.Dttm, &p.Authors, &p.Tags)
+	json, _ := json.Marshal(p)
+	w.Write(json)
 }
 
 func listPosts(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("called listPosts\n")
 	q := cass.Query(`SELECT id, body, dttm, authors, tags FROM posts`)
+	// TODO: loop & build a list to marshal to json
 	p := Post{}
 	q.Scan(&p.Id, &p.Body, &p.Dttm, &p.Authors, &p.Tags)
 	json, _ := json.Marshal(p)
@@ -34,9 +42,5 @@ func listPosts(w http.ResponseWriter, r *http.Request) {
 }
 
 func delPost(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("unimplemented route: delPost()\n")
-}
-
-func getPosts(w http.ResponseWriter, r *http.Request) {
-	fmt.Printf("unimplemented route: getPosts()\n")
+	q := cass.Query(`DELETE FROM posts WHERE id=?`)
 }
