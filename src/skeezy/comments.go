2b8751920c0e5f8ae89682a3c7b86bf7cba1b7a5
Al Tobey <tobert@gmail.com>
2013-10-29 12:15:15 -0700

    Fix silly data structure reuse.

--- src/skeezy/comments.go
+++ src/skeezy/comments.go
@@ -13,8 +13,13 @@ func ListComments(cass *gocql.Session, id uuid.UUID) (chan *Comment) {
 
 	go func() {
 		iq := cass.Query(`SELECT id, parentId, created FROM comments WHERE postId=?`, id.Bytes()).Iter()
-		c := Comment{}
-		for iq.Scan(&c.Id, &c.ParentId, &c.Created) {
+		for {
+			c := Comment{}
+			ok := iq.Scan(&c.Id, &c.ParentId, &c.Created)
+			if !ok {
+				fmt.Printf("End of data? %s\n", ok)
+				break
+			}
 			cc <- &c
 		}
 		if err := iq.Close(); err != nil {
